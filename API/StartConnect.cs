644435e878959c462ef5daa2455a45db43bba5a7
leedavi <dcl@agence-sesame.fr>
2023-07-12 14:42:29 +0200

    IProcessCommand

--- API/StartConnect.cs
+++ API/StartConnect.cs
@@ -6,12 +6,13 @@ using System.Collections.Generic;
 using System.Text;
 using RocketDirectoryAPI;
 using RocketDirectoryAPI.Components;
+using DNNrocketAPI.Interfaces;
 
 namespace RocketSystemWrapTemplate.API
 {
-    public partial class StartConnect : DNNrocketAPI.APInterface
+    public partial class StartConnect : IProcessCommand
     {
-        public override Dictionary<string, object> ProcessCommand(string paramCmd, SimplisityInfo systemInfo, SimplisityInfo interfaceInfo, SimplisityInfo postInfo, SimplisityInfo paramInfo, string langRequired = "")
+        public Dictionary<string, object> ProcessCommand(string paramCmd, SimplisityInfo systemInfo, SimplisityInfo interfaceInfo, SimplisityInfo postInfo, SimplisityInfo paramInfo, string langRequired = "")
         {
             paramCmd = paramCmd.Replace("rocketsystemwraptemplate_", "rocketdirectoryapi_");
             systemInfo.SetXmlProperty("genxml/systemkey", "rocketsystemwraptemplate");
